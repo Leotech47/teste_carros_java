766ab0a0e8bec0abb7c5a34a828279b76379223b
Leonardo <150615719+Leotech47@users.noreply.github.com>
2024-08-07 18:04:08 -0300

    Create Carro.java

---
+++ Carro.java
@@ -0,0 +1,56 @@
+public class Carro {
+    private String placa;
+    private String marca;
+    private String modelo;
+    private String cor;
+
+    // Construtor
+    public Carro(String placa, String marca, String modelo, String cor) {
+        this.placa = placa;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.cor = cor;
+    }
+
+    // Getters
+    public String getPlaca() {
+        return placa;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    // Setters
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    // Método listar
+    public void listar() {
+        System.out.println("Placa: " + this.placa);
+        System.out.println("Marca: " + this.marca);
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Cor: " + this.cor);
+    }
+}
